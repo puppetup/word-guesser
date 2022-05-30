2662cbcaaca5e5993781fbca05a6af4892d6bc18
puppetup <savinohoshizaki@gmail.com>
2022-05-29 18:52:40 -0700

    copied randomizer from stack overflow

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,16 @@ var thirdWord = ['grape'];
 var masterList = [firstWord, secondWord, thirdWord];
 
 // randomly choose var from master
+function randomNoRepeats(array) {
+    var copy = array.slice(0);
+    return function() {
+      if (copy.length < 1) { copy = array.slice(0); }
+      var index = Math.floor(Math.random() * copy.length);
+      var item = copy[index];
+      copy.splice(index, 1);
+      return item;
+    };
+  }
 
 // change word to underscores
 function underScored {
