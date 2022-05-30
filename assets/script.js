d157c38e3720830edbf34e1b9686bd71a25a4492
puppetup <savinohoshizaki@gmail.com>
2022-05-29 18:32:13 -0700

    added master list and comments

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+// words to guess
+var firstWord = ['apple'];
+var secondWord = ['banana'];
+var thirdWord = ['grape'];
+
+// array containing all words
+var masterList = [firstWord, secondWord, thirdWord];
+
+// randomly choose var from master
+
+// change word to underscores
+function underScored {
+
+}
