d52086f7ff8cabe6164115cdcb10c2195f903859
puppetup <savinohoshizaki@gmail.com>
2022-05-29 21:28:19 -0700

    deleted all js and started over. convert word to array

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,11 @@
-// words to guess
-var firstWord = ['apple'];
-var secondWord = ['banana'];
-var thirdWord = ['grape'];
+//List of words
+var guessWord = ['apple','banana','orange','grapes'];
 
-// array containing all words
-var masterList = [firstWord, secondWord, thirdWord];
+// Convert word to array
+var testWord = 'apple'
 
-// randomly choose var from master
-function randomNoRepeats(array) {
-    var copy = array.slice(0);
-    return function() {
-      if (copy.length < 1) { copy = array.slice(0); }
-      var index = Math.floor(Math.random() * copy.length);
-      var item = copy[index];
-      copy.splice(index, 1);
-      return item;
-    };
-  }
+var appleArray = testWord.split('')
 
-// change word to underscores
-function underScored {
+console.log(appleArray)
 
-}
+//
