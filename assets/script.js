8f2b95897274dbba47c71cbe1f10c6ca6d89b806
puppetup <savinohoshizaki@gmail.com>
2022-05-30 19:03:31 -0700

    added two functions and comments

--- assets/script.js
+++ assets/script.js
@@ -8,4 +8,32 @@ var appleArray = testWord.split('')
 
 console.log(appleArray)
 
-//
+// Convert array to underscores
+var arrayUnderscore = appleArray.map((element) => {
+    return element = '_'
+})
+
+console.log(arrayUnderscore)
+
+// Check if value is in original array
+if appleArray.includes(
+
+    //key value
+
+) 
+
+// replace 
+appleArray.forEach(matchLetter)
+
+function matchLetter(keyValue, element) {
+
+    if (keyValue === element) {
+
+        // note the index of matching element
+        // replace underscore with the same index in arrayUnderscore with corresponding letter
+        // display on screen
+        // continue replacing underscores if correct key is pressed
+
+    }
+
+}
